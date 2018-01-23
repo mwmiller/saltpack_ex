038467541d5a11c05882c651ac9191c4bac13946
Matt Miller <mwm@fastmail.com>
2018-01-23 14:00:43 +0200

    Increment version for updated dep fixes

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Saltpack.Mixfile do
 
   def project do
     [app: :saltpack,
-     version: "1.1.2",
+     version: "1.1.3",
      elixir: "~> 1.4",
      name: "saltpack",
      source_url: "https://github.com/mwmiller/saltpack_ex",
@@ -22,7 +22,7 @@ defmodule Saltpack.Mixfile do
     [
       {:basex, "~> 1.0"},
       {:equivalex, "~> 1.0"},
-      {:kcl, "~> 1.0"},
+      {:kcl, ">= 1.0.4"},
       {:msgpax, "~> 2.0"},
       {:earmark, "~> 1.1", only: :dev},
       {:ex_doc, "~> 0.14", only: :dev},
